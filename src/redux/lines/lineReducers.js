a4a7c0bd6f7c7777dc627348c8efad424e916943
puxw <puxw@agilean.cn>
2019-05-25 10:48:22 +0800

    fix key

--- src/redux/lines/lineReducers.js
+++ src/redux/lines/lineReducers.js
@@ -26,7 +26,7 @@ export default function lineReducer(state = initialState, action) {
         case lineActionKeys.addStationFailed:
         case lineActionKeys.deleteStationFailed:
         case lineActionKeys.updateStationKeyFailed:
-        case lineActionKeys.FetchStationsFailed:
+        case lineActionKeys.fetchStationsFailed:
         case lineActionKeys.fetchLinesFailed:
             return state.set('fetching', false).set('errorMsg', action.errorMsg);
         case lineActionKeys.fetchLinesSuccess:
